93c09fe7aad108e196d62dc61b5dc0a427be0c64
Taylor Moore <tjosephmoore@gmail.com>
2017-03-21 07:50:25 +0430

    Imported placeholder art for player, zombie enemy, and scenery

---
+++ Assets/Scripts/Lighting/SpriteEnableLighting.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SpriteEnableLighting : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+
+	void Awake() {
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+	}
+	void Start() {
+		spriteRenderer.receiveShadows = true;
+		spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+	}
+}
