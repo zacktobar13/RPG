9bdcc3b8875b024d63f3e7af2234be2036f90de9
Taylor Moore <taylorgamedev@gmail.com>
2017-03-23 22:16:47 +0430

    Created armor toggling menu to help debug graphical issues with character's armor while creating animations.

---
+++ Assets/Scripts/GUI/Debug/ArmorGraphicalDebugGUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmorGraphicalDebugGUI : MonoBehaviour {
+
+	public GameObject armorMenu;
+	public Toggle[] toggles;
+	bool enableArmorMenu = false;
+
+	void Update () {
+
+		// Detecting input for option to display the menu.
+		if (Input.GetButtonDown("ArmorGraphicDebugToggle")) {
+			enableArmorMenu = !enableArmorMenu;
+		}
+
+		// Taking input handled above for toggling armor menu Game Object.
+		if (enableArmorMenu) {
+			armorMenu.SetActive(true);
+		} else {
+			armorMenu.SetActive(false);
+		}
+	}
+
+	// Logic for Equip All button. Turns every toggle on.
+	public void EquipAll() {
+		foreach (Toggle toggle in toggles) {
+			toggle.isOn = true;
+		}
+	}
+
+	// Logic for Unequip All button. Turns every toggle off.
+	public void UnenquipAll() {
+		foreach (Toggle toggle in toggles) {
+			toggle.isOn = false;
+		}
+	}
+}
---
+++ Assets/Scripts/GUI/Debug/CheckToggleState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckToggleState : MonoBehaviour {
+
+	public SpriteRenderer sprite;
+
+	Toggle toggle;
+
+	// Sets up reference to Toggle component and determines if its checkmark should
+	// be on or off on start up depending on if its corresponding Sprite Renderer
+	// component is initially enabled or disabled.
+	void Awake () {
+
+		toggle = GetComponent<Toggle>();
+
+		if (sprite.enabled) {
+			toggle.isOn = true;
+		} else {
+			toggle.isOn = false;
+		}
+	}
+
+	// Checks every frame if Toggle component's corresponding Sprite Renderer should
+	// be enabled or disabled based on current Toggle state.
+	void Update() {
+		if (toggle.isOn) {
+			sprite.enabled = true;
+		} else {
+			sprite.enabled = false;
+		}
+	}
+
+}
