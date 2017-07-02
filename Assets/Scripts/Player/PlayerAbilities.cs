cd5ed8a19bb2218962ac647d850eee5bafbdd418
Taylor Moore <tjosephmoore@gmail.com>
2017-07-01 19:00:39 -0500

    Basic portals added.

---
+++ Assets/Scripts/Player/PlayerAbilities.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    public GameObject portal;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("CastPortal"))
+        {
+            CastPortal();
+        }
+    }
+
+    void CastPortal()
+    {
+        GameObject newPortal = Instantiate(portal, new Vector3(transform.position.x, transform.position.y, transform.position.z + 4), Quaternion.identity);
+        newPortal.GetComponent<Portal>().SetDestination("Tree Town");
+    }
+}
---
+++ Assets/Scripts/Spells/Portal/Portal.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    Text destinationText;
+
+    public string destination;
+
+    void Awake()
+    {
+        destinationText = GetComponentInChildren<Text>();
+    }
+
+    void Start()
+    {
+        destinationText.text = destination;
+    }
+
+    /// <summary>
+    /// Sets destination to a specific target and updates the portals UI text to display the
+    /// correct destination.
+    /// </summary>
+    /// <param name="dest"></param>
+    public void SetDestination(string dest)
+    {
+        destination = dest;
+        destinationText.text = destination;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PortToDestination();
+        }
+    }
+
+    void PortToDestination()
+    {
+        SceneManager.LoadSceneAsync(destination);
+    }
+}
