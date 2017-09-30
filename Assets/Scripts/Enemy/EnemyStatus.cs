55722827224ec874cc3eb05eb25e07affe80aa62
Taylor Moore <tjosephmoore@gmail.com>
2017-09-29 21:24:48 -0500

    Established basic melee attack for player.

---
+++ Assets/Scripts/Enemy/EnemyStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus : MonoBehaviour {
+
+    public int health;
+	
+	public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log(health);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMelee.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour {
+
+    public GameObject meleeRight;
+    float meleeCooldown = 1f;
+    float lastAttack = 0f;
+
+    void Update () {
+        if ((PlayerInput.attackHorizontal != 0) && (Time.time >= lastAttack + meleeCooldown))
+        {
+            StartCoroutine("MeleeAttack");
+        }
+	}
+
+    IEnumerator MeleeAttack()
+    {
+        meleeRight.SetActive(true);
+        lastAttack = Time.time;
+        yield return new WaitForSeconds(.25f);
+        meleeRight.SetActive(false);
+    }
+
+}
