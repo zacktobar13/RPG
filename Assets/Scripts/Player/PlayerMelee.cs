79cbaca35f16682b07217472fd433c443e49f362
Taylor Moore <tjosephmoore@gmail.com>
2017-10-30 23:15:52 -0500

    Small changes and deleted some meta files.

--- Assets/Scripts/Player/PlayerMelee.cs
+++ Assets/Scripts/Player/PlayerMelee.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMelee : MonoBehaviour {
+public class PlayerMelee : MonoBehaviour
+{
 
     public GameObject meleeRight;
     float meleeCooldown = .5f;
     float lastAttack = 0f;
 
-    void Update () {
+    void Update ()
+    {
         if ((PlayerInput.attackHorizontal != 0) && (Time.time >= lastAttack + meleeCooldown))
         {
             lastAttack = Time.time;
